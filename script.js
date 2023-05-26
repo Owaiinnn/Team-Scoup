ccbce387b9138d2a3168d65007c85dd157cef0cf
Owaiinnn <117091024+Owaiinnn@users.noreply.github.com>
2023-05-26 12:21:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,11 +8,39 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+// Function to validate email
+function validateEmail(event) {
+    const emailInput = document.getElementById('emailInput').value;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(emailInput)) {
+        event.preventDefault();
+
+        const alertDiv = document.createElement('div');
+        alertDiv.classList.add('alert', 'alert-danger', 'mt-3', 'text-white');
+        alertDiv.textContent = 'Please enter a valid email address with a domain.';
+
+        const form = document.querySelector('form');
+        form.appendChild(alertDiv);
+
+        setTimeout(() => {
+            alertDiv.classList.add('fade');
+            alertDiv.style.transition = 'opacity 0.5s';
+            alertDiv.style.opacity = '0';
+            setTimeout(() => {
+                alertDiv.remove();
+            }, 500);
+        }, 5000);
+    }
+}
+
+document.querySelector('form').addEventListener('submit', validateEmail);
+
 const neeknop = document.querySelectorAll('#nee1, #nee2, #nee3, #nee4, #nee5');
 const startknop = document.getElementById('start-knop');
 const fout = document.getElementById('Nopage');
-const volgende = document.querySelectorAll('#volgende1, #volgende2, #volgende3, #volgende4, #volgende5, #volgende6, #volgende7');
-const pages = document.querySelectorAll('#page1, #page2, #page3, #page4, #page5, #page6, #page7');
+const volgende = document.querySelectorAll('#volgende1, #volgende2, #volgende3, #volgende4, #volgende5, #volgende6, #volgende7, #volgende8, #volgende9, #volgende10, #volgende11, #volgende12');
+const pages = document.querySelectorAll('#page1, #page2, #page3, #page4, #page5, #page6, #page7, #page8, #page9, #page10, #page11, #page12, #page13, #page14');
 
 function hideAllPages() {
     pages.forEach((page) => page.style.display = 'none');
@@ -34,5 +62,7 @@ function showNextPage(index) {
 }
 
 startknop.addEventListener('click', start);
-volgende.forEach((vol, index) => vol.addEventListener('click', () => showNextPage(index + 2)));
+volgende.forEach((vol, index) => vol.addEventListener('click', () => {
+    showNextPage(index + 2);
+}));
 neeknop.forEach((element) => element.addEventListener('click', showFout));
